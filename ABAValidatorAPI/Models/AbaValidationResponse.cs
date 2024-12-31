f57eac6427ff97a2096f9557a0a045049b5c1966
MikiKaMiki <mihail.svet.developer@gmail.com>
2024-12-31 03:09:06 +0200

    v2

---
+++ ABAValidatorAPI/Models/AbaValidationResponse.cs
@@ -0,0 +1,35 @@
+﻿using ABAValidatorAPI.Engine;
+
+namespace ABAValidatorAPI.Models
+{
+    public class AbaValidationResponse
+    {
+        public bool IsValid { get; set; }
+
+        public IList<RecordValidationResponse> RecordValidationResults { get; set; } = [];
+    }
+
+    public class RecordValidationResponse
+    {
+        public int LineNumber { get; set; }
+
+        public bool IsValid { get; set; }
+
+        //public string Record { get; set; } = string.Empty;
+
+        public IList<string> Errors { get; set; } = [];
+    }
+
+
+    public static class ToAbaValidationResponse
+    {
+        public static AbaValidationResponse ToResponse(this FileValidationResult context)
+        {
+            var response = new AbaValidationResponse();
+
+            // todo: map
+
+            return response;
+        }
+    }
+}
