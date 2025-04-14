9bfea0478be11af4814a69b1be9e6c2f74f15bd1
MikiKaMiki <mihail.svet.developer@gmail.com>
2025-04-14 22:20:35 +0300

    ABA009 — Setup lunch setting
    serve the application for any IP address

--- ABAValidatorAPI/Engine/Validators/RecordValidators/DescriptiveRecordValidator.cs
+++ ABAValidatorAPI/Engine/Validators/RecordValidators/DescriptiveRecordValidator.cs
@@ -25,40 +25,5 @@ namespace ABAValidatorAPI.Engine
                 ]
             );
         }
-
-        //public RecordValidationResult ValidateRecord(int lineNumber, string line)
-        //{
-        //    var rResult = new RecordValidationResult() { RecordType = RecordType.Descriptive };
-
-        //    ValidateRecord(rResult, lineNumber, line);
-
-        //    if (rResult.IsValid)
-        //    {
-        //        ValidateFields(rResult, lineNumber, line);
-        //    }
-
-        //    rResult.LineNumber = lineNumber;
-            
-        //    return rResult;
-        //}
-
-        //private void ValidateRecord(RecordValidationResult result, int lineNumber, string line)
-        //{
-        //    _recordRule.ValidateRecord(result, line);
-        //}
-
-        //private void ValidateFields(RecordValidationResult recordResult, int lineNumber, string line)
-        //{
-        //    foreach (var fieldType in _fieldTypes)
-        //    {
-        //        FieldValidationResutl fResult = fieldType.ValidateField(line);
-
-        //        if (!fResult.IsValid)
-        //        {
-        //            recordResult.IsValid = false;
-        //            recordResult.FieldValidationResults.Add(fResult);
-        //        }
-        //    }
-        //}
     }
 }
